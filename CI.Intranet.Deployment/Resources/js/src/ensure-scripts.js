4e35df51d7ffbf4cdc75011d9794eb3abcb2d219
Tegan Wilson <tegan.wilson@outlook.com>
2017-06-22 10:47:18 -0600

    Added ensure scripts file.

---
+++ CI.Intranet.Deployment/Resources/js/src/ensure-scripts.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var baseUrl = '/_catalogs/masterpage/morton/js/lib/',
+        angular = 'angular.min.js',
+        angularMap = 'angular.min.js.map';
+
+    if (typeof window.angular == 'undefined') {
+        document.write(unescape('%3Cscript+type%3D%22text%2Fjavascript%22+src%3D%22' + baseUrl + angular + '%22%3E%3C%2Fscript%3E+'));
+        document.write(unescape('%3Cscript+type%3D%22text%2Fjavascript%22+src%3D%22' + baseUrl + angularMap + '%22%3E%3C%2Fscript%3E+'));
+    }
+})();
