77b92c8dbf49c1f1e17c61676c118d6618fa9fd4
Tegan Wilson <tegan.wilson@outlook.com>
2017-07-18 17:14:21 -0500

    Fixed issue with Bartel.js reference.

---
+++ CI.Intranet.Deployment/Resources/js/src/components/my-rss-feeds.js
@@ -0,0 +1,12 @@
+﻿var myApp = angular.module('compassionIntranet'),
+    controllerName = 'myRssFeedsCtrl';
+
+myApp.controller(controllerName, ['$scope', 'rssFeedService', 'COM_CONFIG', function ($scope, rssFeedService, COM_CONFIG) {
+    rssFeedService.getMyRssFeeds().then(function (reponse) {
+        $scope.myFeeds = response;
+    });
+}]).component('myRssFeeds', {
+    template: require('./My-RSS-Feeds.html'),
+    controller:controllerName,
+    controllerAs: 'ctrl'
+});
