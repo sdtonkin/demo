2e6ab60bc30db6c4a6db6c8c7e4b9eb7648a8725
Tegan Wilson <tegan.wilson@outlook.com>
2017-07-21 08:25:04 -0600

    Added tools and apps crud operations.

--- CI.Intranet.Deployment/Resources/js/src/services/tool-bar-service.js
+++ CI.Intranet.Deployment/Resources/js/src/services/tool-bar-service.js
@@ -37,18 +37,67 @@ angular.module('compassionIntranet').service('toolBarService', ['$http', '$q', '
             });
 
         return defer.promise;
+    }
+    function getTools() {
+        var defer = $q.defer();
+        let web = new Web(COM_CONFIG.rootWeb);
+        web.lists.getByTitle(COM_CONFIG.lists.toolbarTools).items
+            .get()
+            .then(function(items){ 
+                var tools = [];
+                for(var i = 0; items.length > i; i++)
+                {
+                    var item = items[i];
+                    var t = {};
+                    t.title = item.Title;
+                    t.url = item.COM_ToolbarUrl.Url;
+                    t.iconUrl = item.COM_ToolbarIconUrl;
 
+                    tools.push(t);
+                }                
+                defer.resolve(f); 
+            });
+
+        return defer.promise;
+    }
+    function addUserTool(userId, toolId) {
+        var defer = $q.defer();
+        let web = new Web(COM_CONFIG.rootWeb);
+        web.lists.getByTitle(COM_CONFIG.lists.userTools).items
+            .add({
+                COM_ToolbarUserId: userId,
+                COM_UserToolbarId: toolId
+            })
+            .then(function(item){ 
+                defer.resolve(item.Id);
+            });
+
+        return defer.promise;
+    }
+    function deleteUserTool(userToolId) {
+        var defer = $q.defer();
+        let web = new Web(COM_CONFIG.rootWeb);
+        web.lists.getByTitle(COM_CONFIG.lists.userTools).items
+            .getById(userToolId)
+            .delete()
+            .then(function(item){ 
+                defer.resolve(true);
+            });
+
+        return defer.promise;
     }
     this.getMyTools = function (userId) {
         var defer = $q.defer();
-        
-
+        getUserToolItems(userId).then(function (tools) {
+            defer.resolve(tools);
+        });
         return defer.promise;
     };
     this.getAllTools = function () {
         var defer = $q.defer();
-        
-
+        getTools().then(function (tools) {
+            defer.resolve(tools);
+        });
         return defer.promise;
     };
 }]);
