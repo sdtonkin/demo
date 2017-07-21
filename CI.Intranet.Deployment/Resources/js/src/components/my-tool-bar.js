9554b26996db3651fc14f155b890964cdc797dc9
Tegan Wilson <tegan.wilson@outlook.com>
2017-07-21 10:37:02 -0600

    Added angular syntax for my tools.

--- CI.Intranet.Deployment/Resources/js/src/components/my-tool-bar.js
+++ CI.Intranet.Deployment/Resources/js/src/components/my-tool-bar.js
@@ -3,7 +3,10 @@
 
 myApp.controller(controllerName, ['$scope', 'toolBarService', 'COM_CONFIG', function ($scope, toolBarService, COM_CONFIG) {
     this.$onInit = function () {
-        
+        var userId = _spPageContextInfo.userId;
+        toolBarService.getMyTools(userId).then(function (response) {
+            $scope.myTools = response;
+        });
     };
 }]).component('myToolBar', {
     template: require('../../includes/Tool-Bar.html'),
