2d6360453f4375f01c77d98bcefbbc476dbff3fa
Tegan Wilson <tegan.wilson@outlook.com>
2017-07-21 08:12:50 -0600

    Removed custom title field for toolbars.

--- CI.Intranet.Deployment/Resources/js/src/services/tool-bar-service.js
+++ CI.Intranet.Deployment/Resources/js/src/services/tool-bar-service.js
@@ -2,17 +2,17 @@
 import { Web } from "sp-pnp-js/lib/sharepoint/webs";
 
 angular.module('compassionIntranet').service('toolBarService', ['$http', '$q', 'COM_CONFIG', function ($http, $q, COM_CONFIG) {
-    function getUserToolItems(user) {
+    function getUserToolItems(userId) {
         var defer = $q.defer();
         let web = new Web(COM_CONFIG.rootWeb);
         web.lists.getByTitle(COM_CONFIG.lists.userTools).items
-            .filter("COM_RssFeedUserId eq '" + user + "'")
+            .filter("COM_ToolbarUser eq '" + userId + "'")
             .get()
             .then(function(data){
                 var promises = new Array();
                 for(var i = 0; data.length > i; i++)
                 {
-                    var p = getRssUrl(data[i].Id);
+                    var p = getTool(data[i].Id);
                     promises.push(p)
                 }
                 $q.all(promises).then(function(response){
@@ -22,89 +22,32 @@ angular.module('compassionIntranet').service('toolBarService', ['$http', '$q', '
 
         return defer.promise;
     }
-    function getRssUrl(id) {
+    function getTool(toolId) {
         var defer = $q.defer();
         let web = new Web(COM_CONFIG.rootWeb);
-        web.lists.getByTitle(COM_CONFIG.lists.rssFeedsListTitle).items
+        web.lists.getByTitle(COM_CONFIG.lists.toolbarTools).items
             .getById(id)
             .get()
             .then(function(item){ 
                 var f = {};
                 f.title = item.Title;
-                f.url = item.COM_RssFeedUrl.Url;
+                f.url = item.COM_ToolbarUrl.Url;
+                f.iconUrl = item.COM_ToolbarIconUrl;
                 defer.resolve(f); 
             });
 
         return defer.promise;
+
     }
-    function getRssFeeds(feeds) {
-        var defer = $q.defer(),
-            promises = [];
-        for (var i = 0; feeds.length > i; i++) {
-            var p = getRssFeed(feeds[i]) ;
-            promises.push(p);
-        }
-        $q.all(promises).then(function (response) {
-            defer.resolve(response);
-        });
-        return defer.promise;
-    }
-    function getRssFeed(feed) {
+    this.getMyTools = function (userId) {
         var defer = $q.defer();
-			
-        $http.jsonp(COM_CONFIG.rssProxyUrl + encodeURIComponent(feed.url)).then(function (response) {
-            if (!response.data) {
-                console.error('Unable to fetch RSS feed from provided URL. Please check the URL.');
-            }
-            var feeds = _.sortBy(response.data.items, function(item){ return item.pubDate; }).reverse();
+        
 
-            for (var i = 0; i < feeds.length; ++i) {
-                var f = feeds[i];
-                f.feedTitle = feed.title;
-                f.pDate = moment().utc(f.pubDate);
-                f.publishedDate = moment(f.pubDate);
-                f.currentTime = moment.utc().format();
-                f.publishedSpanString = getPublishedDurationString(f.pubDate);
-            }
-
-            defer.resolve(feeds);
-        });
         return defer.promise;
-    }
-    function getPublishedDurationString(then) {
-        var ms = moment.utc().diff(moment.utc(then));
-        var d = moment.duration(ms);
-        var t = {};
-        t.hour = d.asHours();
-        t.hourFormat = Math.floor(d.asHours());
-        t.minute = moment.utc(ms).format('m');
-			
-        if (t.hour > 0) {
-            var h = Math.floor(t.hour);
-            return h + 'h';
-        }
-        else if (t.minute == '0')
-            return '';
-        else
-            return t.minute + 'm';
-
-        return t;
-    }
-
-    this.getMyRssFeeds = function (user, articleLimit) {
+    };
+    this.getAllTools = function () {
         var defer = $q.defer();
-        getRssItems(user).then(function (feeds) {
-            getRssFeeds(feeds).then(function (feedContent) {
-                var feedList = [];
-                for (var i = 0; feedContent.length > i; i++) {
-                    var f = {};
-                    f.title = feedContent[i][0].feedTitle;
-                    f.articles = feedContent[i].slice(0, articleLimit);
-                    feedList.push(f);
-                }
-                defer.resolve(feedList);
-            });
-        });
+        
 
         return defer.promise;
     };
