42edd8cd3d47611bd802f5849c3e4014709e100c
Tegan Wilson <tegan.wilson@outlook.com>
2017-08-28 14:04:30 -0600

    Added update to news likes.

--- CI.Intranet.Deployment/Resources/js/src/components/news-page-likes.js
+++ CI.Intranet.Deployment/Resources/js/src/components/news-page-likes.js
@@ -4,38 +4,17 @@ var myApp = angular.module('compassionIntranet'),
 
 myApp.controller(controllerName, ['$scope', 'yammerApiService', function ($scope, yammerApiService) {
     var ctrl = this;
-   
+    var location = window.location.href;
     
     this.$onInit = function () {
-        var messageId;
-        if ($scope.messageId == null) {
-            messageId = getMessageId();
-        } else {
-            messageId = $scope.messageId;
-        }
-        
-        if (messageId == null) {
-            ctrl.likeCount = 0;
-            return;
-        }
-
-        yammerApiService.getLikeCountForMessage(messageId).then(function (response) {
-            ctrl.likeCount = response;
+        yammerApiService.getOpenGraphItemByUrl(location).then(function (response) {
+            console.log(response);
         });
     }
     
-    function getMessageId() {
-        yammerApiService.getMessagesForGroup(COM_CONFIG.yammer.defaultGroupId).then(function (response) {
-            var url = window.location;
-            var thread = _.find(response.messages, function (data) { return _.findIndex(data.attachments, function(a){ return a.name == url; });
-            return thread.liked_by.count;
-        });
-    }
 }]).component('newsPageLikes', {
     template: require('../../includes/News-Page-Likes.html'),
     controller: controllerName,
     controllerAs: 'ctrl',
-    scope: {
-        messageId: '@'
-    }
+    
 });
