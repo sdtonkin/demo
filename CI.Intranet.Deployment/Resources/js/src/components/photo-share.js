14d46bb4086d7844e1519a26c5978fce3ba9a73c
Tegan Wilson <tegan.wilson@outlook.com>
2017-09-05 17:14:21 -0600

    Fixed share your photos link.

--- CI.Intranet.Deployment/Resources/js/src/components/photo-share.js
+++ CI.Intranet.Deployment/Resources/js/src/components/photo-share.js
@@ -11,8 +11,8 @@ app.controller(ctrlName, ['$scope', '$q', '$location', 'photoService', 'COM_CONF
         });
     };
     function displayNewPhotoForm() {
-        var url = '/Lists/MissionPhotos/NewForm.aspx?IsDlg=1';
-        if (newFormUrl) url = newFormUrl;
+        var url = COM_CONFIG.rootWeb + '/_layouts/15/Upload.aspx?List=%7B7CC88BAC-9CC9-4457-A59C-51FF5A21838D%7D&RootFolder=%2Fsites%2Fstage%2FmissionPhotos&ContentTypeId=0x0101009148F5A04DDD49CBA7127AADA5FB792B00AADE34325A8B49CDA8BB4DB53328F21401006059AB8EDD2AB649BE422D937A86AE6B';
+        if (ctrl.newFormUrl) url = newFormUrl;
         var options = SP.UI.$create_DialogOptions();
         options.url = url;
         options.dialogReturnValueCallback = Function.createDelegate(null, null);
