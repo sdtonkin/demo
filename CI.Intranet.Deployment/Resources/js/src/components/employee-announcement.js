31d00d39a7f0eca6f1bb26cc3f067752c870bd8f
Tegan Wilson <tegan.wilson@outlook.com>
2017-09-07 11:53:14 -0600

    Added employee announcement.

--- CI.Intranet.Deployment/Resources/js/src/components/employee-announcement.js
+++ CI.Intranet.Deployment/Resources/js/src/components/employee-announcement.js
@@ -2,11 +2,13 @@
 var myApp = angular.module('compassionIntranet'),
     controllerName = 'employeeAnnouncementCtrl';
 
-myApp.controller(controllerName, ['$scope', 'common', 'appService', 'COM_CONFIG', function ($scope, common, appService, COM_CONFIG) {
+myApp.controller(controllerName, ['$scope', 'common', 'employeeAnnouncementService', 'COM_CONFIG', function ($scope, common, employeeAnnouncementService, COM_CONFIG) {
     var ctrl = this;
     
     this.$onInit = function () {
-        //ctrl.myApps = $scope.$parent.ctrl.myApps;
+        employeeAnnouncementService.getAnnouncements().then(function (data) {
+            ctrl.announcements = data;
+        });
     };
 }]).component('employeeAnnouncement', {
     template: require('../../includes/Employee-Announcement.html'),
