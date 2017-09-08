a0a880bd5ac6a68a3a799aad4082cd90a8aad491
Tegan Wilson <tegan.wilson@outlook.com>
2017-09-08 16:21:50 -0600

    Fixed related news.

--- CI.Intranet.Deployment/Resources/js/src/components/related-news.js
+++ CI.Intranet.Deployment/Resources/js/src/components/related-news.js
@@ -3,17 +3,19 @@ var ctrlName = "relatedNewsCtrl";
 var myApp = angular.module('compassionIntranet');
 
 myApp.controller(ctrlName, ['$scope', 'relatedNewsService', 'COM_CONFIG', function ($scope, relatedNewsService, COM_CONFIG) {
+    var ctrl = this;
 
-    relatedNewsService.getData().then(function (data) {
-        $scope.newsSite = COM_CONFIG.rootWeb + "/news/pages/default.aspx";
-
-        if (data.length === 0) {
-            $scope.notFound = "No related stories found";
-        } else if (data[0].contentType === true) {
-            $scope.news = data;
-        } 
-    });
-
+    this.$onInit = function () {
+        relatedNewsService.getData().then(function (data) {
+            $scope.newsSite = COM_CONFIG.rootWeb + "/news/pages/default.aspx";
+            console.log('related news', data);
+            if (data.length === 0) {
+                $scope.notFound = "No related stories found";
+            } else {
+                $scope.news = data;
+            }
+        });
+    };
 }]).component('relatedNews', {
     template: require('../../includes/Related-News.html'),
     controller: ctrlName,
