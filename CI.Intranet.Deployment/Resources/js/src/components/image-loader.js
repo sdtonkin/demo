060d5dcf6a8bef80c71d8c30c7e80a4b4373aef9
Tegan Wilson <tegan.wilson@outlook.com>
2017-09-28 15:05:43 -0600

    Fixed issue with low bandwidth and article page.

--- CI.Intranet.Deployment/Resources/js/src/components/image-loader.js
+++ CI.Intranet.Deployment/Resources/js/src/components/image-loader.js
@@ -6,7 +6,13 @@ myApp.directive('imageLoader', function () {
     return {
         link: function (scope, element, attrs) {
             scope.$watch('window.lowBandwidth', function () {
-                if (!window.lowBandwidth) {
+                if (!window.lowBa<imgndwidth) {
+                    var tag = '<img';
+                    tag += (scope.imageSrc != null ? ' src="' + scope.imageSrc + '" ' : '');
+                    tag += (scope.imageAltText != null ? ' alt="' + scope.imageAltText + '" ' : '');
+                    tag += (scope.imageCssClass != null ? ' class="' + scope.imageCssClass + "'")
+
+
                     element.append('<img src="' + scope.imageSrc + '" title="' + scope.imageAltText + '" class="' + scope.imageCssClass + '" width="' + scope.imageWidth + '" height="' + scope.imageHeight + '" />');
                 }
             });
