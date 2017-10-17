8025f189e0e6aaa6b6a526260bf8a92ea42141e9
Steve Tonkin <STonkin@rightpoint.com>
2017-10-17 12:02:33 -0600

    Photo and XSL updates for prod

--- CI.Intranet.Deployment/Resources/js/src/components/photo-share.js
+++ CI.Intranet.Deployment/Resources/js/src/components/photo-share.js
@@ -15,7 +15,7 @@ app.controller(ctrlName, ['$scope', '$q', '$location', 'photoService', 'COM_CONF
         });
     };
     function displayNewPhotoForm() {
-        var url = COM_CONFIG.rootWeb + '/_layouts/15/Upload.aspx?List=%7B7CC88BAC-9CC9-4457-A59C-51FF5A21838D%7D&RootFolder=%2Fsites%2Fstage%2FmissionPhotos&ContentTypeId=0x0101009148F5A04DDD49CBA7127AADA5FB792B00AADE34325A8B49CDA8BB4DB53328F21401006059AB8EDD2AB649BE422D937A86AE6B';
+        var url = COM_CONFIG.rootWeb + '/_layouts/15/Upload.aspx?List=%7B729A43A8-8F26-43BB-8D73-EFA69869C460%7D&RootFolder=%2FmissionPhotos&ContentTypeId=0x0101009148F5A04DDD49CBA7127AADA5FB792B00AADE34325A8B49CDA8BB4DB53328F21401003A0E2280F0C17E429BC83C21EA24D44F';
         if (ctrl.newFormUrl) url = newFormUrl;
         var options = {};
         options.url = url;
